ff5941ba61c807a23c47aed057b1438afabedaac
Joshua-K1 <joshuack1996@gmail.com>
2024-01-17 21:44:33 +0000

    x

--- logger.py
+++ logger.py
@@ -1,3 +1,17 @@
-import csv
+import csv 
+import os
 
+
+class Logger:
+   def __init__(self, entry, log_file):
+      self.entry = entry
+      self.log_file = log_file
+
+
+   def write_log(self) -> None:
+      if os.path.exists(self.log_file):
+         print("File exists")
+      else: 
+         print("File does not exist")
+      
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 from chain import Blockain
 from block import Block
+from logger import Logger
 import datetime as dt
 
 # TO DO:
@@ -26,6 +27,10 @@ def main():
 
    # Print as CSV
    print(str(block.index) + "," + str(block.timestamp) + "," + str(block.data) + "," + str(block.previous_hash))
+   log_entry = str(block.index) + "," + str(block.timestamp) + "," + str(block.data) + "," + str(block.previous_hash)
+
+   logger = Logger(log_entry, "./log/chain_log.csv")
+   logger.write_log()
 
 
 
