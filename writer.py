36d5a2178edcd09aee8103a3c612f406bfe9127d
Joshua-K1 <joshuack1996@gmail.com>
2024-01-17 22:10:32 +0000

    x

--- writer.py
+++ writer.py
@@ -11,10 +11,15 @@ class Writer:
    def write_chain(self) -> None:
       if os.path.exists(self.log_file) and os.path.getsize(self.log_file) == 0:
          print("File exists and has a size of 0")
-         
+
+         with open(self.log_file, mode='a', newline='') as file: 
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(['Block', 'Timestamp', 'Data', 'Previous Hash'])
+
       elif os.path.exists(self.log_file) and os.path.getsize(self.log_file) > 0: 
          print("File exists and has content")
          # Write next row of file
+
       else:
          print("File does not exist")
          # End
