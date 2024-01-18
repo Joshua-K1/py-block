13e3e01145011b619c45f6c71853c9cf23d04ddf
Joshua-K1 <joshuack1996@gmail.com>
2024-01-18 20:50:57 +0000

    x

--- logger.py
+++ logger.py
@@ -1,11 +1,15 @@
 import os
+import datetime
 
 #@TODO: Parser argument for specifying a log file location
 
 class Logger:
-   def __init__(self, location=None):
+   def __init__(self, location=None, date=None):
       if location is None: location = "./log/log.json"
       self.location = location
+
+      if date is None: date = datetime.datetime.now()
+      self.date = date
 
    def log_file_check(self) -> bool:
       if os.path.exists(str(self.location)):
